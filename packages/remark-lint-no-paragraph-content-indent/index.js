1392178b0f9210aeef507a19e2651c3cfbeba18f
Joaquín Serna <BubuAnabelas@users.noreply.github.com>
2018-01-07 02:45:44 -0300

    remark-lint-no-paragraph-content-indent: fix typo
    Closes GH-167.

--- packages/remark-lint-no-paragraph-content-indent/index.js
+++ packages/remark-lint-no-paragraph-content-indent/index.js
@@ -4,7 +4,7 @@
  * @license MIT
  * @module no-paragraph-content-indent
  * @fileoverview
- *   Warn when warn when the content in paragraphs are indented.
+ *   Warn when the content in paragraphs are indented.
  *
  * @example {"name": "valid.md"}
  *
